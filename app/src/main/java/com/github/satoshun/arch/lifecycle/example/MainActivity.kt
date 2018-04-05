eeddd037e4fae8fe3c7b20bcab65dc6f811d1786
SatoShun <shun.sato1@gmail.com>
2018-04-05 12:54:51 +0000

    fix example path

--- app/src/main/java/com/github/satoshun/arch/lifecycle/example/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/arch/lifecycle/example/MainActivity.kt
@@ -1,10 +1,9 @@
-package com.github.satoshun.arch.lifecycle
+package com.github.satoshun.arch.lifecycle.example
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_act)
