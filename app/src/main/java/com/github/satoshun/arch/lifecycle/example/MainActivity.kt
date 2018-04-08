07040eed06c017d59f500481af0b63c7c0e7d51c
SatoShun <shun.sato1@gmail.com>
2018-04-08 11:49:27 +0000

    impl ContextWrapper.bindService

--- app/src/main/java/com/github/satoshun/arch/lifecycle/example/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/arch/lifecycle/example/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.github.satoshun.arch.lifecycle.example
 
 import android.animation.Animator
+import android.app.Service
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Binder
 import android.os.Bundle
 import android.os.Handler
+import android.os.IBinder
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.github.satoshun.arch.lifecycle.animation.start
+import com.github.satoshun.arch.lifecycle.content.bindService
 import com.github.satoshun.arch.lifecycle.os.postDelayed
+import java.util.*
+
 
 private const val FINISH_MILLS = 5000L
 private const val BASE_MILLS = 10000L
@@ -45,8 +55,41 @@ class MainActivity : AppCompatActivity() {
       TODO("never call")
     }
 
+    val intent = Intent(this, LocalService::class.java)
+    bindService(
+        this,
+        intent,
+        object : ServiceConnection {
+          override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+          }
+
+          override fun onServiceDisconnected(name: ComponentName?) {
+          }
+        },
+        Context.BIND_AUTO_CREATE
+    )
+
     Handler().postDelayed({
       finish()
     }, FINISH_MILLS)
+  }
+}
+
+
+/**
+ * https://developer.android.com/guide/components/bound-services.html
+ */
+class LocalService : Service() {
+  // Binder given to clients
+  private val binder = LocalBinder()
+  // Random number generator
+  private val generator = Random()
+
+  inner class LocalBinder : Binder() {
+    val service: LocalService get() = this@LocalService
+  }
+
+  override fun onBind(intent: Intent): IBinder {
+    return binder
   }
 }
