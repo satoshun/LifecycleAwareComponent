8cf5eb52b430c8c22acf70870b707b4e610e3688
SatoShun <shun.sato1@gmail.com>
2018-04-14 23:30:53 +0000

    fix indent, add todo comment

--- lifecycleaware-gms-location/src/main/java/com/github/satoshun/arch/lifecycle/gms/location/FusedLocationProviderClients.kt
+++ lifecycleaware-gms-location/src/main/java/com/github/satoshun/arch/lifecycle/gms/location/FusedLocationProviderClients.kt
@@ -13,6 +13,11 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.tasks.Task
 
+/**
+ * Version of [FusedLocationProviderClient.requestLocationUpdates]
+ *
+ * todo
+ */
 @SuppressLint("MissingPermission")
 @RequiresPermission(anyOf = [
   Manifest.permission.ACCESS_COARSE_LOCATION,
