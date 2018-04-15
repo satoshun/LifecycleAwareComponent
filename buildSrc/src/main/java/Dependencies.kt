110ba6b5a120c4a37615f4ea9862e3f81f485baa
SatoShun <shun.sato1@gmail.com>
2018-04-15 10:29:59 +0000

    use buildSrc in some modules

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,6 +2,7 @@ object Vers {
   val compile_sdk = 27
   val min_sdk = 15
   val target_sdk = 27
+  val agp = "3.1.1"
 
   val kotlin = "1.2.31"
   val support_lib = "27.1.1"
@@ -14,4 +15,14 @@ object Libs {
   val viewmodel = "android.arch.lifecycle:viewmodel:1.1.1"
   val livedata = "android.arch.lifecycle:livedata:1.1.1"
   val gms = "com.google.android.gms:play-services-location:15.0.0"
+
+  val junit = "junit:junit:4.12"
+  val support_test = "com.android.support.test:runner:1.0.1"
+  val espresso = "com.android.support.test.espresso:espresso-core:3.0.1"
+  val arch_test = "android.arch.core:core-testing:1.1.1"
+
+  val appcompat = "com.android.support:appcompat-v7:${Vers.support_lib}"
+  val support_v4 = "com.android.support:support-v4:${Vers.support_lib}"
+
+  val truth = "com.google.truth:truth:0.39"
 }
