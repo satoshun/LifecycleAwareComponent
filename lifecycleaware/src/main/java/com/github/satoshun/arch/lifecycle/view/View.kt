fc4ed66092599612efaa0508309ea861fc4e8a4a
SatoShun <shun.sato1@gmail.com>
2018-04-22 11:51:44 +0000

    View: add method for Lifecycle

--- lifecycleaware/src/main/java/com/github/satoshun/arch/lifecycle/view/View.kt
+++ lifecycleaware/src/main/java/com/github/satoshun/arch/lifecycle/view/View.kt
@@ -17,9 +17,23 @@ fun View.postDelayed(
     lifecycleEvent: Lifecycle.Event = owner.correspondingEvent(),
     action: () -> Unit
 ): Boolean {
+  return postDelayed(owner.lifecycle, delayInMillis, lifecycleEvent, action)
+}
+
+/**
+ * Version of [View.postDelayed]
+ *
+ * todo
+ */
+fun View.postDelayed(
+    lifecycle: Lifecycle,
+    delayInMillis: Long,
+    lifecycleEvent: Lifecycle.Event = lifecycle.correspondingEvent(),
+    action: () -> Unit
+): Boolean {
   val runnable = Runnable { action() }
   val result = postDelayed(runnable, delayInMillis)
-  owner.lifecycle.addObserver(LifecycleAwareObserver(
+  lifecycle.addObserver(LifecycleAwareObserver(
       lifecycleEvent,
       action = {
         removeCallbacks(runnable)
