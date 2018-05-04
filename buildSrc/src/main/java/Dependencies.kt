07c98c5005665ff489b2de362a048bb930887f96
SatoShun <shun.sato1@gmail.com>
2018-05-04 09:33:14 +0000

    update agp and ktlint

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,11 +2,11 @@ object Vers {
   val compile_sdk = 27
   val min_sdk = 15
   val target_sdk = 27
-  val agp = "3.1.2"
+  val agp = "3.2.0-alpha13"
 
   val kotlin = "1.2.41"
   val support_lib = "27.1.1"
-  val ktlint = "0.21.0"
+  val ktlint = "0.23.0"
 }
 
 object Libs {
